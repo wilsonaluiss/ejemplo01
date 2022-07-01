0e40faf5358e4eb528e128f65af20bdbe1bfee0a
wilsonaluiss <wluis@miumg.edu.gt>
2022-07-01 16:16:36 -0600

    cambio de ruta

--- src/main/java/ejemplo01/demo/DemoApplication.java
+++ src/main/java/ejemplo01/demo/DemoApplication.java
@@ -19,7 +19,7 @@ public class DemoApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("*").allowedHeaders("*");
+				registry.addMapping("/**").allowedOrigins("https://ejemplo01-front.herokuapp.com").allowedMethods("*").allowedHeaders("*");
                                 
                                 
                                 
